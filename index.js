e365f6c9b1f8fc6a239b42180d495e41c7e4ffbe
chuckytato <chuckytato08@gmail.com>
2025-08-31 16:13:31 -0300

    manejo de prisma

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(david72)
+import express from "express"
+import registerRoutes from './routes/index.js'
+
+const servidor = express();
+
+servidor.use(express.json());
+
+registerRoutes(servidor);
+
+servidor.listen(3000, () =>{
+console.log("El servidor esta en el puerto 3000");
+});
+
